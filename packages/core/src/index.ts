fb67ea3ea2c33092fa7b82bb67e7d6b7fb7fefb0
Peter Szerzo <szerzo.peter@gmail.com>
2020-11-03 15:47:31 +0100

    Simplify core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,10 @@
-import qs from "querystring";
 import axios from "axios";
 
 interface Config {
   apiKey: string;
   botId: string;
   journeyId?: string;
+  contactId: string;
 }
 
 export interface VoiceCompass {
@@ -12,71 +12,6 @@ export interface VoiceCompass {
 }
 
 const apiUrl = "https://api.voicecompass.ai/v1";
-
-const parseContactInfo = () => {
-  const { search } = window.location;
-  const normalizedSearch =
-    search && search[0] === "?" ? search.substring(1) : search;
-  const parsed = qs.parse(normalizedSearch);
-
-  const contactId = parsed.cid
-    ? Array.isArray(parsed.cid)
-      ? parsed.cid.join("")
-      : parsed.cid
-    : localStorage.getItem("vc-cid");
-
-  if (contactId) {
-    localStorage.setItem("vc-cid", contactId);
-  }
-
-  const fromReferrer = parsed.r
-    ? Array.isArray(parsed.r)
-      ? parsed.r.join("")
-      : parsed.r
-    : localStorage.getItem("vc-r");
-
-  if (fromReferrer) {
-    localStorage.setItem("vc-r", fromReferrer);
-  }
-
-  const email = parsed.email
-    ? Array.isArray(parsed.email)
-      ? parsed.email.join("")
-      : parsed.email
-    : localStorage.getItem("vc-email");
-
-  if (email) {
-    localStorage.setItem("vc-email", email);
-  }
-
-  const phone = parsed.phone
-    ? Array.isArray(parsed.phone)
-      ? parsed.phone.join("")
-      : parsed.phone
-    : localStorage.getItem("vc-phone");
-
-  if (phone) {
-    localStorage.setItem("vc-phone", phone);
-  }
-
-  const voice = parsed.voice
-    ? Array.isArray(parsed.voice)
-      ? parsed.voice.join("")
-      : parsed.voice
-    : localStorage.getItem("vc-voice");
-
-  if (voice) {
-    localStorage.setItem("vc-voice", voice);
-  }
-
-  return {
-    contactId,
-    email,
-    phone,
-    fromReferrer,
-    voice,
-  };
-};
 
 interface StepData {
   stepId: string;
@@ -88,8 +23,6 @@ interface StepData {
 }
 
 export const create = (config: Config): VoiceCompass => {
-  const info = parseContactInfo();
-
   const client = axios.create({
     baseURL: apiUrl,
     timeout: 15000,
@@ -106,31 +39,15 @@ export const create = (config: Config): VoiceCompass => {
       return;
     }
 
-    // skip step if no contactId is set
-    if (!info.contactId) {
-      return;
-    }
-
     client
       .post("/track", {
-        stepId: stepData.stepId,
-        contactId: info.contactId,
-        voice: stepData.voice,
-        end: stepData.end,
-        escalate: stepData.escalate,
-        payload: stepData.payload,
+        ...stepData,
+        contactId: config.contactId,
         botId: config.botId,
         journeyId: stepData.journeyId || config.journeyId,
-        language: info.voice,
       })
       .then(() => {
         stepId = stepData.stepId;
-
-        // remove the voice from cache, if the call is ending
-        // this is to ensure consistency when a new call is placed
-        if (stepData.end) {
-          localStorage.removeItem("vc-voice");
-        }
       });
   };
 
