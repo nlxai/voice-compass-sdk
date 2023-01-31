4bc65cebf9f14bf602964db9a74a6ec2f4b5b69f
Peter Szerzo <szerzo.peter@gmail.com>
2023-01-31 09:54:59 +0100

    Add environment variables to SDK test script

---
+++ packages/core/src/test.ts
@@ -0,0 +1,24 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_VC_DEV_API_KEY: string;
+  readonly VITE_VC_DEV_JOURNEY_ASSISTANT_ID: string;
+  readonly VITE_VC_DEV_JOURNEY_ID: string;
+}
+
+import { create } from "./index";
+
+// @ts-ignore
+console.log(import.meta.env.VITE_VC_DEV_API_KEY);
+
+const vc = create({
+  dev: true,
+  // @ts-ignore
+  apiKey: import.meta.env.VITE_VC_DEV_API_KEY,
+  contactId: "",
+  // @ts-ignore
+  journeyId: import.meta.env.VITE_VC_DEV_JOURNEY_ID,
+  // @ts-ignore
+  journeyAssistantId: import.meta.env.VITE_VC_DEV_JOURNEY_ASSISTANT_ID,
+});
+
+vc.runWizard();
