e546a62f32f6c37b560c1ad10999059b92d801d3
Peter Szerzo <szerzo.peter@gmail.com>
2023-02-03 17:23:24 +0100

    Cleanup

---
+++ packages/point-and-click/src/icons.tsx
@@ -0,0 +1,19 @@
+import { h } from "preact";
+
+export const TriggerIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor">
+    <path d="M9.4 16.6 4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0 4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"></path>
+  </svg>
+);
+
+export const RemoveCircleOutlineIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor">
+    <path d="M7 11v2h10v-2H7zm5-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path>
+  </svg>
+);
+
+export const AutoFixHighIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor">
+    <path d="M7.5 5.6 10 7 8.6 4.5 10 2 7.5 3.4 5 2l1.4 2.5L5 7zm12 9.8L17 14l1.4 2.5L17 19l2.5-1.4L22 19l-1.4-2.5L22 14zM22 2l-2.5 1.4L17 2l1.4 2.5L17 7l2.5-1.4L22 7l-1.4-2.5zm-7.63 5.29a.9959.9959 0 0 0-1.41 0L1.29 18.96c-.39.39-.39 1.02 0 1.41l2.34 2.34c.39.39 1.02.39 1.41 0L16.7 11.05c.39-.39.39-1.02 0-1.41l-2.33-2.35zm-1.03 5.49-2.12-2.12 2.44-2.44 2.12 2.12-2.44 2.44z"></path>
+  </svg>
+);
