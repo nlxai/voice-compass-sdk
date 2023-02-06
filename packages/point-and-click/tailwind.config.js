291e4bcb3ec1adcb86d9fa32e2b8c06d4f60a452
RomanistNLX <raman.smuniou@nlx.ai>
2023-02-06 17:26:52 +0400

    fix: blincking and position horizontal

--- packages/point-and-click/tailwind.config.js
+++ packages/point-and-click/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        firstlyTransparent: {
+          '0%, 50%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        firstlyTransparent: 'firstlyTransparent .2s',
+      },
+    },
   },
   plugins: [],
 };
